bacb5ce3ac705167dcdc849922e4f586aa0e8373
sfabrizzio <sfabrizzio@hotmail.com>
2016-09-04 11:25:31 -0500

    commit

--- src/main/java/net/viralpatel/spring/config/AppConfig.java
+++ src/main/java/net/viralpatel/spring/config/AppConfig.java
@@ -3,23 +3,29 @@ package net.viralpatel.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 
 @Configuration
 @EnableWebMvc
+
 @ComponentScan(basePackages = "net.viralpatel.spring")
 public class AppConfig extends WebMvcConfigurerAdapter {
-	@Bean
+	
+	//Jsp
+	/*@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
+		viewResolver.setSuffix(".html");
 
 		return viewResolver;
 	}
@@ -28,5 +34,45 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+//resources mapping
+*/	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+ 
+	
+	//Thymeleaf ........
+	@Bean
+	@Description("Thymeleaf Template Resolver")
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
 
+		return templateResolver;
+	}
+
+	@Bean
+	@Description("Thymeleaf Template Engine")
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+
+		return templateEngine;
+	}
+
+	@Bean
+	@Description("Thymeleaf View Resolver")
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		
+		return viewResolver;
+		
+	}
+
+	//....Thymeleaf
 }
--- src/main/java/net/viralpatel/spring/controller/HelloController.java
+++ src/main/java/net/viralpatel/spring/controller/HelloController.java
@@ -1,17 +1,60 @@
 package net.viralpatel.spring.controller;
 
+import java.util.List;
+
+
+
+
+
+
+
+
+import net.viralpatel.*;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class HelloController {
-
-	@GetMapping("/hello")
+	
+	@RequestMapping("/")
+	
 	public String hello(Model model) {
 
 		model.addAttribute("name", "John Doe");
 
-		return "welcome";
+		return "taxi";
+	}
+@RequestMapping("/taxi")
+	
+	public String taxi1(Model model) {
+
+		model.addAttribute("name", "John Doe");
+
+		return "taxi";
+	}
+@RequestMapping("/registro")
+
+public String taxi2(Model model) {
+
+	model.addAttribute("name", "John Doe");
+
+	return "denuncia";
+}
+
+	
+@RequestMapping("/listas")
+	
+	public String hellod(Model model) {
+
+		model.addAttribute("name", "John Doe");
+
+		return "listas";
 	}
 }
